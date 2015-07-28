9347315ad1bd521f6b98b0389ddfb4de5872ba35
TonyZhang94 <879833043@qq.com>
2015-07-28 14:14:23 +0800

    submit

--- App/Runtime/Cache/Home/598c228dfaede5b5832bc50d20526093.php
+++ App/Runtime/Cache/Home/598c228dfaede5b5832bc50d20526093.php
@@ -1,19 +1,19 @@
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link href="__PUBLIC__/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link href="__PUBLIC__/css/commonstyle.css" rel="stylesheet" />
-		<link href="__PUBLIC__/css/style.css" rel="stylesheet"/>
-		<script type="text/javascript" src="__PUBLIC__/script/plugins/jquery-1.11.2.js"></script>
-		<script type="text/javascript" src="__PUBLIC__/script/goodsPayment.js"></script>
-		<script src="__PUBLIC__/bootstrap/js/bootstrap.min.js"></script>
+		<link href="/foru/Public/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/foru/Public/css/commonstyle.css" rel="stylesheet" />
+		<link href="/foru/Public/css/style.css" rel="stylesheet"/>
+		<script type="text/javascript" src="/foru/Public/script/plugins/jquery-1.11.2.js"></script>
+		<script type="text/javascript" src="/foru/Public/script/goodsPayment.js"></script>
+		<script src="/foru/Public/bootstrap/js/bootstrap.min.js"></script>
 		
 		<!--li-->
-		<link rel="stylesheet" href="__PUBLIC__/css/style_li.css" />
-		<!-- // <script type="text/javascript" src="__PUBLIC__/script/pay_li.js"></script> -->
+		<link rel="stylesheet" href="/foru/Public/css/style_li.css" />
+		<!-- // <script type="text/javascript" src="/foru/Public/script/pay_li.js"></script> -->
 	</head>
 	<body data-spy="scroll" data-target="#nav-side">
 		<div class="public-top-layout" style="background-color: #fff">
@@ -27,19 +27,19 @@
 					<a class="text-special" href="">手机For优</a>
 				</div>
 				<div class="quick-menu">
-					欢迎光临<span class="text-special">ForU</span>校园超市，请 <a class="text-special" href="{:U('Login/index')}">登录</a><a class="text-special" href="{:U('Login/register')}">注册</a>
+					欢迎光临<span class="text-special">ForU</span>校园超市，请 <a class="text-special" href="<?php echo U('Login/index');?>">登录</a><a class="text-special" href="<?php echo U('Login/register');?>">注册</a>
 					<span> </span>
 				</div>
 				
 			</div>
 		</div>
 		<div id="index-header" >
-			<!-- <form action="{:U('Person/payAtOnce')}" method="POST"> -->
+			<!-- <form action="<?php echo U('Person/payAtOnce');?>" method="POST"> -->
 				<div class="container header-bottom">
 					<div id="header-botton-wrapper">
 						<div id="log-wrapper" class="fl">
 							<div id="header-logo" class="fl">
-								<img src="__PUBLIC__/img/logo.png" class="fl">
+								<img src="/foru/Public/img/logo.png" class="fl">
 								<span class="text-special fl"><p>For优<br><span class="bold inline-block">为你更好的生活</span></span>
 							</div>
 							<div id="header-search" class="fl">
@@ -59,7 +59,7 @@
 
 							<div id="shopping-cart" class="drop-down" >
 								<div class="drop-down-left">
-									<img src="__PUBLIC__/img/icon/shopping-cart.png" alt="">	
+									<img src="/foru/Public/img/icon/shopping-cart.png" alt="">	
 									<a target="_blank" href="">购物车 &gt;&gt;</a>
 								</div>
 								<div class="drop-down-layer">
@@ -91,7 +91,7 @@
 							</li>
 							
 							<li>
-								<img src="__PUBLIC__/img/icon/location.png" alt="">
+								<img src="/foru/Public/img/icon/location.png" alt="">
 								<select id="location" >
 								  	<option value ="volvo">苏州大学独墅湖校区</option>
 								  	<option value ="saab">苏州大学阳澄湖校区</option>
@@ -126,24 +126,22 @@
 							</tr>
 						</thead>
 						<tbody>
-							<foreach name="address" item="v">
-								<tr>
+							<?php if(is_array($address)): foreach($address as $key=>$v): ?><tr>
 									<td>
-										<input type="radio" name="information" value="{$v.rank}" class="main-wrapper-1-radio"/>
-										收货人:<span>{$v.receiverName}</span><br />
-										&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;联系电话：<span>{$v.customer_phone}</span>
+										<input type="radio" name="information" value="<?php echo ($v["rank"]); ?>" class="main-wrapper-1-radio"/>
+										收货人:<span><?php echo ($v["receiverName"]); ?></span><br />
+										&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;联系电话：<span><?php echo ($v["customer_phone"]); ?></span>
 									</td>
 									<td>
-										<span>{$v.address}</span>
+										<span><?php echo ($v["address"]); ?></span>
 									</td>
 									<td>
 										<input type="button" value="修改" class="main-wrapper-1-btn-revise"/>
 										<input type="button" value="删除路径" class="main-wrapper-1-btn-delete"/>
-										<input class="phone-none none" value="{$v.phone}"> 
-										<input class="rank-none none"  value="{$v.rank}">
+										<input class="phone-none none" value="<?php echo ($v["phone"]); ?>"> 
+										<input class="rank-none none"  value="<?php echo ($v["rank"]); ?>">
 									</td>
-								</tr>
-							</foreach>
+								</tr><?php endforeach; endif; ?>
 							<!-- <tr>
 								<td>
 									<input type="radio" name="information" class="main-wrapper-1-radio"/>
@@ -168,7 +166,7 @@
 				
 				
 				<div class="wrapper main-wrapper-2">
-					<form action="{:U('Person/addOrReviseSave')}" method="POST">
+					<form action="<?php echo U('Person/addOrReviseSave');?>" method="POST">
 						<div>
 							<label for="li_person">收货人：</label>
 							<input type="text" name="user-name" id="userName" class="person"/>
@@ -199,7 +197,7 @@
 						</div>
 					</form>
 				</div>
-			<form action="{:U('Person/payAtOnce')}" method="POST">
+			<form action="<?php echo U('Person/payAtOnce');?>" method="POST">
 				<div class="wrapper main-wrapper-3">
 					<table class="wrapper">
 						<colgroup>
@@ -214,14 +212,14 @@
 							<tr>
 								<td>
 									<input type="radio" name="pay_way"/>
-									<img src="__PUBLIC__/img/zhifubao.png" alt="" />
+									<img src="/foru/Public/img/zhifubao.png" alt="" />
 									支付宝支付
 								</td>
 							</tr>
 							<tr>
 								<td>
 									<input type="radio" name="pay_way"/>&nbsp&nbsp&nbsp
-									<img src="__PUBLIC__/img/weixin.png" alt="" />
+									<img src="/foru/Public/img/weixin.png" alt="" />
 									&nbsp&nbsp&nbsp&nbsp微信支付
 								</td>
 							</tr>
@@ -250,36 +248,34 @@
 						<tbody>				
 							<tr class="order-info-head">
 								<td colspan="3">
-									订单编号：<span>{$orderInfo.order_id}</span>
-									提交时间：<span>{$orderInfo.order_time}</span>	
+									订单编号：<span><?php echo ($orderInfo["order_id"]); ?></span>
+									提交时间：<span><?php echo ($orderInfo["order_time"]); ?></span>	
 								</td>
 								<td colspan="2">
 									
 								</td>	
 							</tr>
 							<!-- 追加1 删除2 付款3 取消4 确认5  评价6-->
-							<foreach name="goodsInfo" item="v">
-								<tr class="order-info-detailed">
+							<?php if(is_array($goodsInfo)): foreach($goodsInfo as $key=>$v): ?><tr class="order-info-detailed">
 									<td>
-										<img class="fl" src="{$v.foodImg}" alt="">
+										<img class="fl" src="<?php echo ($v["foodImg"]); ?>" alt="">
 										<div class="fl">
-											<span class='b'>{$v.foodName}</span><p>
-											<span>{$v.message}</span><p>
-											<span>共<span>{$v.number}</span>件商品</span><p>
+											<span class='b'><?php echo ($v["foodName"]); ?></span><p>
+											<span><?php echo ($v["message"]); ?></span><p>
+											<span>共<span><?php echo ($v["number"]); ?></span>件商品</span><p>
 										</div>		
 									</td>
 									<td class='b'><span class="gray move">颜色分类：黄色</span></td>
 									<td class='b'>
-										<span class="gray move"><span>￥{$v.d_price}</span><span><del>原价：{$v.price}</del></span></span>
+										<span class="gray move"><span>￥<?php echo ($v["d_price"]); ?></span><span><del>原价：<?php echo ($v["price"]); ?></del></span></span>
 									</td >
 									<td>
-										<span>{$v.number}</span>				
+										<span><?php echo ($v["number"]); ?></span>				
 									</td>
 									<td class="b">
-										<span class="move"><span class="red_price">￥{$v.discountPrice}</span><span class="gray"><del>原价：{$v.totalPrice}</del></span></span>
+										<span class="move"><span class="red_price">￥<?php echo ($v["discountPrice"]); ?></span><span class="gray"><del>原价：<?php echo ($v["totalPrice"]); ?></del></span></span>
 									</td>
-								</tr>	
-							</foreach>		
+								</tr><?php endforeach; endif; ?>		
 						</tbody>
 					</table>
 				</div>
@@ -342,8 +338,8 @@
 				
 				<div class="wrapper main-wrapper-7 ">
 					<div class="main-wrapper-7-main">
-						<span class="zhe"><span>合计：</span><span class="price">￥</span><span class="price">{$price['discountPrice']}</span><span class="price">元</span><br /></span>
-						<span class="yuan"><del>原价：￥<span>{$price['totalPrice']}</span>元</del></span><br />
+						<span class="zhe"><span>合计：</span><span class="price">￥</span><span class="price"><?php echo ($price['discountPrice']); ?></span><span class="price">元</span><br /></span>
+						<span class="yuan"><del>原价：￥<span><?php echo ($price['totalPrice']); ?></span>元</del></span><br />
 						<input type="submit" value="立即支付"/>
 					</div>
 				</div>
@@ -357,7 +353,7 @@
 				<ul>
 					<li>
 						<dl>
-							<dd><img src="__PUBLIC__/img/footer/footer1.png" alt=""></dd>
+							<dd><img src="/foru/Public/img/footer/footer1.png" alt=""></dd>
 							<dt>
 								<div>正品保障</div>
 								<div>全场正品，行货保障</div>
@@ -366,7 +362,7 @@
 					</li>
 					<li>
 						<dl>
-							<dd><img src="__PUBLIC__/img/footer/footer2.png" alt=""></dd>
+							<dd><img src="/foru/Public/img/footer/footer2.png" alt=""></dd>
 							<dt>
 								<div>新手指南</div>
 								<div>快速登录，无需注册</div>
@@ -375,7 +371,7 @@
 					</li>
 					<li>
 						<dl>
-							<dd><img src="__PUBLIC__/img/footer/footer3.png" alt=""></dd>
+							<dd><img src="/foru/Public/img/footer/footer3.png" alt=""></dd>
 							<dt>
 								<div>货到付款</div>
 								<div>货到付款，安心便捷</div>
@@ -385,7 +381,7 @@
 					</li>
 					<li>
 						<dl>
-							<dd><img src="__PUBLIC__/img/footer/footer4.png" alt=""></dd>
+							<dd><img src="/foru/Public/img/footer/footer4.png" alt=""></dd>
 							<dt>
 								<div>维修保障</div>
 								<div>服务保证，全国联保</div>
@@ -394,7 +390,7 @@
 					</li>
 					<li>
 						<dl>
-							<dd><img src="__PUBLIC__/img/footer/footer5.png" alt=""></dd>
+							<dd><img src="/foru/Public/img/footer/footer5.png" alt=""></dd>
 							<dt>
 								<div>无忧退货</div>
 								<div>无忧退货，7日尊享</div>
@@ -403,7 +399,7 @@
 					</li>
 					<li>
 						<dl>
-							<dd><img src="__PUBLIC__/img/footer/footer6.png" alt=""></dd>
+							<dd><img src="/foru/Public/img/footer/footer6.png" alt=""></dd>
 							<dt>
 								<div>会员权益</div>
 								<div>会员升级，尊贵特权</div>
